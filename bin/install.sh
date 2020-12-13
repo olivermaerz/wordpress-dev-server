d7646fd330e7b4fbeeb8498745817d25e708e25b
Oliver Maerz <github@olivermaerz.com>
2020-12-13 11:40:40 -0300

    Add workaround for mac

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-main_dir="$(dirname $(dirname $(realpath $0)) )"
+realpath2() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
+main_dir="$(dirname $(dirname $(realpath2 "$@" $0)) )"
 cd "$main_dir" || exit
 
 # Download and untar WordPress
@@ -16,3 +29,4 @@ chmod +x bin/*.sh
 printf "\nSetup complete. You can now navigate to http://localhost:8080/ in your browser.\n"
 printf "\nTo stop the development server run './stop.sh' in this directory.\n"
 printf "\nTo start the development server in the future run './start.sh'.\n"
+
